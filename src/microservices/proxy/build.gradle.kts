6fb4e8ad2f5ea3002fd8ee5a9c6f31ec0c7e81b5
Aleksei Malyshev <malyshev@atlas-kard.ru>
2025-11-23 22:12:14 +0300

    task2.1

---
+++ src/microservices/proxy/build.gradle.kts
@@ -0,0 +1,43 @@
+val camel_version: String by project
+
+plugins {
+    kotlin("jvm") version "1.9.25"
+    kotlin("plugin.spring") version "1.9.25"
+    id("org.springframework.boot") version "3.5.8"
+    id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com.cinemaabyss.proxy"
+version = "0.0.1-SNAPSHOT"
+description = "proxy"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(17)
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.apache.camel.springboot:camel-spring-boot-starter:$camel_version")
+    implementation("org.apache.camel.springboot:camel-servlet-starter:${camel_version}")
+    implementation("org.apache.camel.springboot:camel-http-starter:${camel_version}")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+kotlin {
+    compilerOptions {
+        freeCompilerArgs.addAll("-Xjsr305=strict")
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
---
+++ src/microservices/proxy/src/main/kotlin/com/cinemaabyss/proxy/MainRoute.kt
@@ -0,0 +1,78 @@
+package com.cinemaabyss.proxy
+
+import jakarta.annotation.PostConstruct
+import org.apache.camel.builder.RouteBuilder
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Component
+
+@Component
+class MainRoute : RouteBuilder() {
+
+    @Value("\${MONOLITH_URL}") lateinit var monolithUrl: String
+    @Value("\${MOVIES_SERVICE_URL}") lateinit var moviesServiceUrl: String
+    @Value("\${EVENTS_SERVICE_URL}") lateinit var eventsServiceUrl: String
+    @Value("\${GRADUAL_MIGRATION}") lateinit var gradualMigration: String
+    @Value("\${MOVIES_MIGRATION_PERCENT}") lateinit var moviesMigrationPrc: String
+
+    private var distibutionRatiosMovie : Pair<Int, Int> = Pair(1, 0);
+
+    private fun gcd(a: Int, b: Int): Int {
+        return if (b == 0) a else gcd(b, a % b)
+    }
+
+    fun getDistribution(percent: Int): Pair<Int, Int> {
+        require(percent in 0..100) { "Процент должен быть от 0 до 100" }
+
+        return when (percent) {
+            0 ->  Pair(1, 0)
+            100 ->  Pair(0, 1)
+            else -> {
+                val gcd = gcd(percent, 100)
+                val numerator = percent / gcd
+                val denominator = 100 / gcd
+                Pair(denominator - numerator, numerator)
+            }
+        }
+    }
+
+    @PostConstruct
+    fun init(){
+
+        try {
+            if (gradualMigration.toBoolean()) {
+
+                log.info("GRADUAL_MIGRATION enabled")
+
+                val percent = moviesMigrationPrc.toInt()
+                distibutionRatiosMovie = getDistribution(percent)
+
+            }
+        } catch (ex: Exception) {
+            log.error(ex.message, ex)
+        }
+
+        log.info("MOVIES_MIGRATION distribution = $distibutionRatiosMovie")
+    }
+
+    override fun configure() {
+
+        from("servlet:///*")
+            .choice()
+            .`when`(header("CamelHttpPath").startsWith("/api/movies"))
+                .to("direct:movies")
+            .otherwise()
+                .to("direct:default")
+
+        from("direct:movies")
+            .routeId("moviesproxy")
+            .loadBalance().weighted(true, "${distibutionRatiosMovie.first},${distibutionRatiosMovie.second}")
+                .to("$monolithUrl?bridgeEndpoint=true")
+                .to("$moviesServiceUrl?bridgeEndpoint=true")
+            .end()
+
+        from("direct:default")
+            .routeId("allproxy")
+            .to("$monolithUrl?bridgeEndpoint=true")
+
+    }
+}
